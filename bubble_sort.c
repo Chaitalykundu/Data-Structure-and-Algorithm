c48f94573f24a9ac1524eea927e939565382bd8d
Chaitaly Kundu <chaitaly.slg01@gmail.com>
2019-12-29 21:27:35 +0530

    bubble sort

---
+++ bubble_sort.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+
+void BubbleSort(int a[],int n)
+{
+    int i,j,temp;
+    for(i=0;i<n-1;i++)
+    {
+        for(j=0;j<n-i-1;j++)
+        {
+            if(a[j]>a[j+1])
+            {
+                temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+            }
+        }
+    }
+
+}
