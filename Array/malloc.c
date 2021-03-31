bbb295c4a33438c524c38e223d804b50fe8597ff
Chaitaly Kundu <chaitaly.slg01@gmail.com>
2021-03-31 09:52:50 +0530

    add file

---
+++ Array/malloc.c
@@ -0,0 +1,36 @@
+/* Author : Chaitaly Kundu
+   Date: 31-03-2021 */
+
+/* WAP to see the entered numbered and their locations using malloc */
+#include <stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int n,i;
+    printf("Enter the number: ");
+    scanf("%d",&n);
+
+    int *ptr = (int *)malloc(n*sizeof(int));
+
+    if(ptr == NULL)
+    {
+        printf("Memory not found");
+    }
+    else{
+        printf("Enter the integers: ");
+        for(i=0;i<n;i++)
+        {
+            scanf("%d",ptr+i);
+        }
+
+        printf("Entered numbers and their locations are: ");
+        for(i=0;i<n;i++)
+        {
+            printf("\n%d  ",*(ptr+i));
+            printf("%d  ", (ptr+i));
+
+        }
+    }
+    return 0;
+}
