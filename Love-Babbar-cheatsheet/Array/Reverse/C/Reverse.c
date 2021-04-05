17f96e0326fe25fcc5a0a95f674372eeb9744806
Chaitaly Kundu <chaitaly.slg01@gmail.com>
2021-04-05 16:48:27 +0530

    Add file

---
+++ Love-Babbar-cheatsheet/Array/Reverse/C/Reverse.c
@@ -0,0 +1,46 @@
+/* Author : Chaitaly Kundu
+   Date: 05-04-2021 */
+
+/* WAP to reverse an array */
+#include<stdio.h>
+
+
+int reverse(int a[], int n)
+{
+    int i,temp;
+    for(i=0;i<n/2;i++)
+    {
+        temp=a[i];
+        a[i]=a[n-1-i];
+        a[n-1-i]=temp;
+    }
+}
+
+int printArray(int a[], int n)
+{
+    int i;
+    for(i=0;i<n;i++)
+    {
+        printf("%d\t",a[i]);
+    }
+}
+
+int main()
+{
+    int n,i;
+    int a[10];
+    printf("Enter the no of elements: ");
+    scanf("%d",&n);
+    printf("\nEnter the elements: ");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    printf("\nArray Elements are: \t");
+    printArray(a,n);
+    reverse(a,n);
+    printf("\nAfter reversing: \t");
+    printArray(a,n);
+
+    return 0;
+}
---
+++ Love-Babbar-cheatsheet/Array/Reverse/Python/reverse.py
@@ -0,0 +1,16 @@
+#   Author : Chaitaly Kundu
+#   Date: 05-04-2021 
+
+# WAP to reverse an array 
+
+
+def reverse(A):
+    print(A[::-1])
+A=[1,2,3,4,5,6,7]
+
+print("Array elements are: ")
+print(A)    
+
+
+print("After reversing: ")
+reverse(A)
