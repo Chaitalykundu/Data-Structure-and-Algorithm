079c7adbf7567dd9d557f628fd15b04bf59c659b
Chaitaly Kundu <chaitaly.slg01@gmail.com>
2021-04-13 01:25:59 +0530

    add file

--- Preliminaries/loc_and_max_value.c
+++ Preliminaries/loc_and_max_value.c
@@ -8,17 +8,18 @@ Date: 12th march, 2021 */
 int main()
 {
     int a[10] = {3,5,2,6,4,9,4,6,2,1};
-    int i, max=a[0],loc;
+    int n = sizeof(a)/sizeof(a[0]);
+    int i, max=a[0],loc=0;
     /*printf("Enter the number of elements: ");
     scanf("%d",n);
-
+code 
     printf("Enter the elements: ");;
     for(i=0;i<n;i++)
     {
         scanf("%d",a[i]);
     }*/
 
-    for(i=0;i<5;i++)
+    for(i=0;i<n;i++)
     {
         if(max<a[i+1])
         {
---
+++ sorting/Insertion_sort/CPP/Using_for_loop.c
@@ -0,0 +1,61 @@
+/* Author: Chaitaly Kundu
+Date: 12-04-2021 */
+
+#include <iostream>
+using namespace std;
+
+void InsertionSort(int a[], int n)
+{
+    int i, j, temp;
+    for(i=1;i<n;i++)
+    {
+        temp=a[i];
+        for(j=i-1;j>=0;j--)
+        {
+            if(a[j]>temp)
+            {
+                a[j+1]=a[j];
+            }
+            else{
+                break;
+            }
+        }
+        a[j]=temp;
+    }
+}
+
+void print_elements(int a[], int n)
+{
+    int e;
+    for(e=0;e<n;e++)
+    {
+        cout << "\t" << a[e];
+    }
+    cout << endl;
+
+}
+int main() {
+	// your code goes here
+
+	int n,i;
+    cout << "Enter the no of elements: " << endl;
+    cin >> n;
+
+    int a[n];
+
+    cout << "Elements are: " << endl;
+    for(i=0;i<n;i++)
+    {
+        cin >> a[i];
+    }
+
+    cout << "Before Sorting: ";
+    print_elements(a,n);
+
+    InsertionSort(a,n);
+
+    cout << "After Sorting: ";
+    print_elements(a,n);
+
+	return 0;
+}
