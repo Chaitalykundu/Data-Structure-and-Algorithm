25babde544f09f534ab4c3475045b12d5c6334c7
Chaitaly Kundu <chaitaly.slg01@gmail.com>
2021-05-26 10:37:01 +0530

    Add commit

--- Searching/Linear-Search/Using_recursion.c
+++ Searching/Linear-Search/Using_recursion.c
@@ -4,23 +4,27 @@ Date: 21st May, 2021 */
 /* Linear Search */
 #include<stdio.h>
 
-int LinearSearch(int a[], int n, int s)
+int LinearSearch(int a[], int n, int i, int s)
 {
-    int i;
 
-    if(i==a[i])
+    if(i>=n)
+        return 0;
+    else if(s==a[i])
+        return i;
+    else
+        return LinearSearch(a, n, i+1,s);
 }
 
 int main()
 {
-    int n,s,result;
+    int n,s,result, i=0;
     int a[]={1,2,3,4,5,6,7,8,9,10};
 
     n = sizeof(a)/ sizeof(a[0]);
     printf("Enter the element to be searched: ");
     scanf("%d",&s);
 
-    result = LinearSearch(a,n,s);
+    result = LinearSearch(a,n,i,s);
     printf(" %d is in index %d", s, result);
     return 0;
 }
