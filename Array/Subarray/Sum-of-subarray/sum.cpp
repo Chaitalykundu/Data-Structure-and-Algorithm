7edb6b729860d076218b9eee468350d543c114f0
Chaitalykundu <chaitaly.slg01@gmail.com>
2021-09-05 11:22:04 +0530

    Add commit

---
+++ Array/Subarray/Sum-of-subarray/sum.cpp
@@ -0,0 +1,33 @@
+/* Author : Chaitaly Kundu
+   Date : 05-09-2021*/
+
+#include<iostream>
+using namespace std;
+
+class sum
+{
+public:
+    void sum_subarray(int a[], int n)
+    {
+        for(int i=0;i<n;i++)
+        {
+            int current_sum = 0;
+            for(int j=i;j<n;j++)
+            {
+                current_sum +=a[j];
+                cout << current_sum << endl;
+            }
+        }
+    }
+};
+
+int main(){
+
+    int n = 5;
+    int a[n] = {1,4,3,2,5};
+
+    sum obj;
+    obj.sum_subarray(a,n);
+
+    return 0;
+}
